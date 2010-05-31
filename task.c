741e094d665ab86daa13e607dfa661958c85f7b0
Daniel Prokesch <daniel.prokesch@gmail.com>
2010-05-31 13:20:58 +0200

    task.c added, only comments contained up to now

---
+++ task.c
@@ -0,0 +1,20 @@
+
+/**
+ * Functions for task management
+ */
+
+#include "task.h"
+
+/**
+ * Create a task
+ * - function to execute
+ */
+
+
+/**
+ * Destroy a task
+ */
+
+/**
+ * Join with a task
+ */
