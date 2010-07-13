cc312789b3ecba754aa258d40ffe9061df822231
Daniel Prokesch <daniel.prokesch@gmail.com>
2010-07-13 13:38:14 +0200

    added atomic test benchmark, written timing functions as inline

---
+++ tests/atomic/bench.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+#include "../../timing.h"
+
+
+#define NUM_THREADS 4
+#define MAX_INC (400000000 / NUM_THREADS)
+
+static int counter;
+
+static inline int fetch_and_inc(volatile int *cnt)
+{
+  int tmp = 1;
+  asm volatile("lock\n\t" "xadd%z0 %0,%1"
+             : "=r" (tmp), "=m" (*cnt)
+             : "0" (tmp), "m" (*cnt)
+             : "memory", "cc");
+  return tmp;
+}
+
+void *thread_naive(void *arg)
+{
+  int i;
+  for (i=0; i<MAX_INC; i++) counter += 1;
+  pthread_exit(NULL);
+}
+
+
+void *thread_builtin(void *arg)
+{
+  int i, z;
+  for (i=0; i<MAX_INC; i++) z = __sync_fetch_and_add(&counter, 1);
+  pthread_exit(NULL);
+}
+
+
+void *thread_asm(void *arg)
+{
+  int i,z;
+  for (i=0; i<MAX_INC; i++) {
+    z = fetch_and_inc(&counter);
+#if NUM_THREADS == 1
+    assert(z==i);
+#endif
+  }
+  pthread_exit(NULL);
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  int n;
+  pthread_t th[NUM_THREADS];
+  double msec;
+  timing_t tt;
+
+  printf("Atomic counter test, incrementing %d times.\n", MAX_INC);
+
+  counter = 0;
+  printf("Testing naive...\n");
+  TimingStart(&tt);
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_create(&th[n], NULL, thread_naive, NULL);  
+  }
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_join(th[n], NULL);  
+  }
+  TimingEnd(&tt);
+  msec = TimingToMSec(&tt);
+  printf("Result: %d, in %f msec.\n", counter, msec);
+
+  counter = 0;
+  printf("Testing builtin...\n");
+  TimingStart(&tt);
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_create(&th[n], NULL, thread_builtin, NULL);  
+  }
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_join(th[n], NULL);  
+  }
+  TimingEnd(&tt);
+  msec = TimingToMSec(&tt);
+  printf("Result: %d, in %f msec.\n", counter, msec);
+
+
+  counter = 0;
+  printf("Testing inline asm...\n");
+  TimingStart(&tt);
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_create(&th[n], NULL, thread_asm, NULL);  
+  }
+  for (n=0; n<NUM_THREADS; n++) {
+    (void) pthread_join(th[n], NULL);  
+  }
+  TimingEnd(&tt);
+  msec = TimingToMSec(&tt);
+  printf("Result: %d, in %f msec.\n", counter, msec);
+
+  return 0;
+}
--- timing.h
+++ timing.h
@@ -1,17 +1,128 @@
 #ifndef _TIMING_H_
 #define _TIMING_H_
 
+/*
+ * Link with librt:
+ *   -lrt
+ */
 #include <time.h>
+
+#include "debug.h"
 
 typedef struct timespec timing_t;
 
-extern void TimingStart(timing_t *t);
-extern void TimingEnd(timing_t *t);
-extern void TimingAdd(timing_t *t, const timing_t *val);
-extern void TimingSet(timing_t *t, const timing_t *val);
-extern void TimingZero(timing_t *t);
-extern void TimingExpAvg(timing_t *t, const timing_t *last, const float alpha);
 
 
+/**
+ * Start timing
+ *
+ * Gets current timestamp and stores it in t
+ */
+static inline void TimingStart(timing_t *t)
+{
+  (void) clock_gettime(CLOCK_MONOTONIC, t);
+  /*TODO check if CLOCK_MONOTONIC is available */
+}
+
+/**
+ * End timing, store the elapsed time in t
+ *
+ * Precond: TimingStart() was called on t
+ */
+static inline void TimingEnd(timing_t *t)
+{
+  timing_t start, end;
+  
+  /* get end time  */
+  (void) clock_gettime(CLOCK_MONOTONIC, &end);
+  /* store start time */
+  start = *t;
+
+  DBG("start %d , %d", start.tv_sec, start.tv_nsec);
+  DBG("end   %d , %d", end.tv_sec,   end.tv_nsec);
+
+  /* calculate elapsed time to t,
+   * assuming end > start and *.tv_nsec < 1000000000L
+   */
+  if (end.tv_nsec < start.tv_nsec) {
+    start.tv_nsec -= 1000000000L;
+    start.tv_sec  += 1L;
+  }
+  t->tv_nsec = end.tv_nsec - start.tv_nsec;
+  t->tv_sec  = end.tv_sec  - start.tv_sec;
+
+  DBG("diff  %d , %d", t->tv_sec, t->tv_nsec);
+}
+
+/**
+ * Add a time to another
+ *
+ * Adds val to t, val is not modified
+ */
+static inline void TimingAdd(timing_t *t, const timing_t *val)
+{
+  t->tv_sec  += val->tv_sec;
+  t->tv_nsec += val->tv_nsec;
+  /* normalize */
+  if (t->tv_nsec > 1000000000L) {
+    t->tv_nsec -= 1000000000L;
+    t->tv_sec  += 1L;
+  }
+}
+
+/**
+ * Set a time
+ */
+static inline void TimingSet(timing_t *t, const timing_t *val)
+{
+  t->tv_sec  = val->tv_sec;
+  t->tv_nsec = val->tv_nsec;
+}
+
+/**
+ * Clear a time to 0
+ */
+static inline void TimingZero(timing_t *t)
+{
+  t->tv_sec  = 0;
+  t->tv_nsec = 0;
+}
+
+/**
+ * Calculate the exponential average
+ *
+ * Updates t by last with weight factor alpha
+ * tnew = last*alpha + told*(1-alpha)
+ * 
+ * Precond: alpha in [0,1]
+ */
+static inline void TimingExpAvg(timing_t *t,
+  const timing_t *last, const float alpha)
+{
+  timing_t wlast, whist;
+
+  wlast.tv_nsec = alpha*last->tv_nsec;
+  wlast.tv_sec  = alpha*last->tv_sec;
+
+  whist.tv_nsec = (1-alpha)*t->tv_nsec;
+  whist.tv_sec =  (1-alpha)*t->tv_sec;
+
+  t->tv_nsec = wlast.tv_nsec + whist.tv_nsec;
+  t->tv_sec  = wlast.tv_sec  + whist.tv_sec;
+  /* normalize */
+  if (t->tv_nsec > 1000000000L) {
+    t->tv_nsec -= 1000000000L;
+    t->tv_sec  += 1L;
+  }
+}
+
+
+/**
+ * Get milliseconds from timing
+ */
+static inline double TimingToMSec(timing_t *t)
+{
+  return (((double)t->tv_sec) * 1000.0f) + (((double)t->tv_nsec) / 1000000.0f);
+}
 
 #endif /* _TIMING_H_ */
