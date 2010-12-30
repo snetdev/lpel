68a301987c434101e621fff3b4449e630d311f52
Daniel Prokesch <daniel.prokesch@gmail.com>
2010-12-30 15:54:50 +0100

    Implemented explicit management of task_t structs (allocation and recycling on the worker)

--- task.h
+++ task.h
@@ -37,6 +37,26 @@ typedef enum {
 } taskstate_blocked_t;
 
 
+typedef struct {
+  int flags;
+  int stacksize;
+} taskattr_t;
+
+
+struct lpel_taskreq_t {
+  unsigned int    uid;
+  lpel_taskfunc_t func;
+  void *          inarg;
+  taskattr_t      attr;
+  /* for joining: */
+  /*
+  atomic_t refcnt;
+  task_t *waiter;
+  void **joinval;
+  */
+};
+
+
 
 /**
  * TASK CONTROL BLOCK
@@ -44,11 +64,12 @@ typedef enum {
 struct lpel_task_t {
   /** intrinsic pointers for organizing tasks in a list*/
   lpel_task_t *prev, *next;
-  unsigned int uid;         /** unique identifier */
-  lpel_taskattr_t attr;     /** attributes */
-  taskstate_t state;        /** state */
+  unsigned int uid;    /** unique identifier */
+  taskattr_t attr;     /** attributes */
+  taskstate_t state;   /** state */
   taskstate_blocked_t blocked_on; /** on which event the task is waiting */
 
+  lpel_taskreq_t *request;      /** task request that created this task */
   workerctx_t *worker_context;  /** worker context for this task */
 
   /**
@@ -69,6 +90,7 @@ struct lpel_task_t {
   lpel_stream_desc_t *dirty_list;
 
   /* CODE */
+  pthread_mutex_t lock; /** context of the task*/
   coroutine_t ctx;      /** context of the task*/
   lpel_taskfunc_t code; /** function of the task */
   void *inarg;          /** input argument  */
@@ -76,11 +98,14 @@ struct lpel_task_t {
 
 
 
+lpel_task_t *_LpelTaskCreate( void);
+void         _LpelTaskDestroy( lpel_task_t *t);
 
-void _LpelTaskAssign(  lpel_task_t *t, workerctx_t *wc);
-void _LpelTaskCall(    lpel_task_t *t);
-void _LpelTaskBlock(   lpel_task_t *ct, taskstate_blocked_t block_on);
-void _LpelTaskDestroy( lpel_task_t *t);
+void _LpelTaskReset( lpel_task_t *t, lpel_taskreq_t *req);
+void _LpelTaskUnset( lpel_task_t *t);
+
+void _LpelTaskCall(  lpel_task_t *t);
+void _LpelTaskBlock( lpel_task_t *ct, taskstate_blocked_t block_on);
 
 
 #endif /* _TASK_H_ */
