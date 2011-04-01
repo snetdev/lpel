9dfb92f3e05e7e394b1f2c17661339f0f59366c1
Daniel Prokesch <daniel.prokesch@gmail.com>
2011-04-01 15:12:51 +0200

    Fixed amd64 ctx switch, stack based.

--- tests/exec_stack/stack.c
+++ tests/exec_stack/stack.c
@@ -3,52 +3,49 @@
 #include <stdio.h>
 
 #include <malloc.h>
-#include <unistd.h>
 
-#include <pcl.h>
-
-
-typedef struct {
-  int dummy;
-  coroutine_t coro;
-  void* stack;
-} tcb_t;
-
-static tcb_t *tcb;
+#include "arch/mctx.h"
 
 
-void coro_func(void *arg)
+static mctx_t ctx_main, ctx1, ctx2;
+
+#define STACKSIZE 4096
+
+void func1(void *arg)
 {
-  int a;
-  printf("Address of a: %p\n", &a);
-  printf("Difference tcb-a: %ld\n", (long)tcb-(long)&a);
-  co_resume();
+  printf("Function 1, arg: %p\n", arg);
+  mctx_switch( &ctx1, &ctx2 );
+  printf("Function 2 again, arg: %p\n", arg);
+  mctx_switch( &ctx1, &ctx2 );
 }
 
-void main(int argc, char **argv)
+void func2(void *arg)
 {
-  int pagesize = sysconf(_SC_PAGESIZE);
-  int stacksize = 4*pagesize;
-
-  void *mem = memalign(pagesize, stacksize);
-  
-  /* following is unsafe, as*/
-  tcb = mem+stacksize-sizeof(tcb_t);
-
-  printf("Test exec stack\n");
-
-  printf("Pagesize: %d\n", pagesize);
-  printf("Memory ptr: %p\n", mem);
-
-  printf("Memory start of tcb: %p\n", (void *) tcb);
+  printf("Function 2, arg: %p\n", arg);
+  mctx_switch( &ctx2, &ctx1 );
+  printf("Function 2 again, arg: %p\n", arg);
+  mctx_switch( &ctx2, &ctx_main );
+}
 
 
-  printf("Size of TCB: %ld\n", sizeof(tcb_t));
 
-  tcb->coro = co_create(coro_func, NULL, mem, stacksize-sizeof(tcb_t));
-  co_call(tcb->coro);
+
+
+int main(int argc, char **argv)
+{
+  void *st1 = valloc(STACKSIZE);
+  void *st2 = valloc(STACKSIZE);
+ 
+  mctx_create( &ctx1, func1, (void*)0x100, st1, STACKSIZE);
+  mctx_create( &ctx2, func2, (void*)0x200, st2, STACKSIZE);
+
+
+  printf("Function main calling func1\n");
+  mctx_switch( &ctx_main, &ctx1 );
+  printf("Function main again\n");
 
 
   printf("Test finished\n");
+  return 0;
 }
 
