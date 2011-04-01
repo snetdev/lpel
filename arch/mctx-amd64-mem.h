44d365cd891fd39a390ae47d32f41bd3ce093448
Daniel Prokesch <daniel.prokesch@gmail.com>
2011-04-01 20:54:16 +0200

    Added header file for pure stack-based ctx switch.

--- arch/mctx-amd64-mem.h
+++ arch/mctx-amd64-mem.h
@@ -1,7 +1,11 @@
 
 
 
-typedef void* mctx_t;
+typedef struct {
+  void *regs[10];
+  void  *sk_addr;
+  size_t sk_size;
+} mctx_t;
 
 void ctx_init_internal(void**, void*, void(*)(void*), void*);
 void ctx_swap_internal(void**, void**);
@@ -9,7 +13,9 @@ void ctx_swap_internal(void**, void**);
 
 static inline int mctx_create(mctx_t *mctx, void *func, void *arg, char *sk_addr, long sk_size)
 {
-  ctx_init_internal(mctx, sk_addr+sk_size, func, arg);
+  mctx->sk_addr = sk_addr + sk_size;
+  mctx->sk_size = sk_size;
+  ctx_init_internal(mctx->regs, sk_addr+sk_size, func, arg);
   return 0;
 }
 
@@ -18,7 +24,7 @@ static inline int mctx_create(mctx_t *mctx, void *func, void *arg, char *sk_addr
 
 static inline void mctx_switch(mctx_t *octx, mctx_t *nctx)
 {
-  ctx_swap_internal(octx, nctx);
+  ctx_swap_internal(octx->regs, nctx->regs);
 }
 
 
