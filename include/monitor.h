6c7c549e29ea0e082ea94716f6926f235332742b
Nguyen Vu Thien Nga <nguyenvuthiennga@gmail.com>
2011-10-12 17:36:00 +0100

    - monitor.h: define macros to activate/deactivate monitoring info

---
+++ include/monitor.h
@@ -0,0 +1,20 @@
+#ifndef _MONITOR_H_
+#define _MONITOR_H_
+// macros to activate logging or logging mode(s)
+#define USE_LOGGING
+//#define NO_TASK_EVENT_LOGGING
+//#define NO_USER_EVENT_LOGGING
+
+
+#ifdef USE_LOGGING
+	#ifndef NO_TASK_EVENT_LOGGING
+		#define USE_TASK_EVENT_LOGGING
+		#ifndef NO_USER_EVENT_LOGGING
+			#define USE_USER_EVENT_LOGGING
+		#endif
+	#endif
+#endif
+
+
+
+#endif // _MONITOR_H
