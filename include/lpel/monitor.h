6ff085683d7232d3473c2c17d1ddf92dcf4118b4
Nguyen Vu Thien Nga <nguyenvuthiennga@gmail.com>
2011-12-19 14:50:47 +0000

    - separate logging macro across LPEL and SNET levels   + for LPEL: worker, task, stream. Macros are defined in "lpel/monitor.h"   + for SNET: message. Macros are defined in "moninfo.h"

--- include/lpel/monitor.h
+++ include/lpel/monitor.h
@@ -5,17 +5,11 @@
 //#define NO_TASK_EVENT_LOGGING
 //#define NO_USER_EVENT_LOGGING
 
-/*
- * since the overhead of monitoring control is too small --> currently not
- * support these options
+
 #ifdef USE_LOGGING
 	#ifndef NO_TASK_EVENT_LOGGING
 		#define USE_TASK_EVENT_LOGGING
-		#ifndef NO_USER_EVENT_LOGGING
-			#define USE_USER_EVENT_LOGGING
-		#endif
 	#endif
 #endif
-*/
 
 #endif // _MONITOR_H
